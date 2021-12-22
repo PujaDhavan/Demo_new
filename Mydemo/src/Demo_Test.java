c8bc9286bd228cc56b05665f4380f7213df62077
puja <pujadhavan124@gmail.com>
2021-12-22 13:53:49 +0530

    decos

--- Mydemo/src/Demo_Test.java
+++ Mydemo/src/Demo_Test.java
@@ -6,7 +6,7 @@ public class Demo_Test {
 		System.out.println("Hiiiiii");
 		System.out.println("Changes made by SatiSh");
 		System.out.println("Accept my pool request");
-		System.out.println("hi kishu");
+		
 		
 		System.out.println("dhdhhdhh");
 		System.out.println("nitu");
