5277e90c740c3691b7d7e8de0fe04876729b1310
puja <pujadhavan124@gmail.com>
2021-12-22 13:27:40 +0530

    hello commit

--- Mydemo/src/Demo_Test.java
+++ Mydemo/src/Demo_Test.java
@@ -6,13 +6,7 @@ public class Demo_Test {
 		System.out.println("Hiiiiii");
 		System.out.println("Changes made by SatiSh");
 		System.out.println("Accept my pool request");
-		System.out.println("bye Java");
-		System.out.println("Hi Java");
-		System.out.println("Hi Testing");
-		System.out.println("Hello Team");
-		System.out.println("Changing from master- need to pull in eclipse");
-		System.out.println("Changing form eclipse- need to push from eclipse");
-		System.out.println("Creating Master Branch");
+		System.out.println("Hello Dear kishu ");
 	}
 
 }
